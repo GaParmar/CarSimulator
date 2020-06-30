fdb5588e3f9a47c93c5f1c90b7ebd03929348027
gaparmar <gparmar@ucsd.edu>
2020-06-30 08:39:14 -0700

    configuration moved to the top

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CarSimulator
 Collect data from car simulator for dagger
+
+# use env "pytorch_sac_ae"
--- main.py
+++ main.py
@@ -5,32 +5,41 @@ from controller import *
 import gym
 import gym_donkeycar
 
+"""
+Configuration parameters
+"""
+sim_path      = "sim/DonkeySimLinux/donkey_sim.x86_64"
+env_name      = "donkey-mountain-track-v0"
+host          = "127.0.0.1"
+port          = 9092
+wc_port       = 8887
+wc_mode       = "user"
+refresh_rate  = 20
+
+
+
 # initialize the donkey car gym
-sim_path = "sim/DonkeySimLinux/donkey_sim.x86_64"
-env_name = "donkey-mountain-track-v0"
-host = "127.0.0.1"
-port = 9092
 env  = gym.make(env_name, exe_path=sim_path,
                 host=host, port=port)
 obs = env.reset()
-
-web_controller = LocalWebController(port=8887, mode="user")
-t = Thread(target=web_controller.update, args=())
-t.daemon = True
-# start the loop
-t.start()
-
-rate_hz = 10
+# initialize the web controller
+wc = LocalWebController(port=wc_port, mode=wc_mode)
+wc_thread = Thread(target=wc.update, args=())
+wc_thread.daemon = True
+wc_thread.start()
 
 while True:
     start_time = time.time()
-    outputs = web_controller.run_threaded(img_arr=None)
+    outputs = wc.run_threaded(img_arr=None)
     angle, throttle, _, _ = outputs
     angle = angle*5.0
+    # throttle = 0.5
     action = np.array([angle, throttle])
     curr_obs, reward, done, info = env.step(action)
-    print(outputs, info)
+    print(action)
 
-    sleep_time = 1.0 / rate_hz - (time.time() - start_time)
+    sleep_time = 1.0 / refresh_rate - (time.time() - start_time)
     if sleep_time > 0.0:
         time.sleep(sleep_time)
+    else:
+        print("going over")
